3c3fce725c776de086f7c8073723284af4723e39
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-10 14:28:03 +0200

    Selected elements for review total, returning user, and user name displays

---
+++ utilis.ts
@@ -0,0 +1,3 @@
+const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
+const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
+const userNameDisplay = document.querySelector('#user') as HTMLElement
