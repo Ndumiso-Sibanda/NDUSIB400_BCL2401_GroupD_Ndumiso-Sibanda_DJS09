bb532ff7c9b6f60216b475be7dae70a46d970f81
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-10 14:35:44 +0200

    Implemented showDetails function

--- utilis.ts
+++ utilis.ts
@@ -24,3 +24,11 @@ export function populateUser(isReturning : boolean, userName: string ) {
     }
     userNameDisplay.innerHTML = userName
 }
+
+export function showDetails(value: boolean | Permissions, element : HTMLDivElement, price: number) {
+    if (value) {
+        const priceDisplay = document.createElement('div')
+        priceDisplay.innerHTML = price.toString() + '/night'
+        element.appendChild(priceDisplay)
+    }
+}
