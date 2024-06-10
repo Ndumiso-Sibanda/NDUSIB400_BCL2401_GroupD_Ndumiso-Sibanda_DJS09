9554a7334a51073c206eb4b5b88f2b8b7afa819d
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-10 14:30:08 +0200

    Defined LoyaltyUser enum

--- utilis.ts
+++ utilis.ts
@@ -6,3 +6,9 @@ enum Permissions {
     ADMIN = 'ADMIN', 
     READ_ONLY = 'READ_ONLY'
 }
+
+enum LoyaltyUser {
+    GOLD_USER = 'GOLD_USER',
+    SILVER_USER = 'SILVER_USER',
+    BRONZE_USER = 'BRONZE_USER'
+}
