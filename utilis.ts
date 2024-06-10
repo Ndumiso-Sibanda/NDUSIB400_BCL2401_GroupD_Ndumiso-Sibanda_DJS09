df8eeb2c406d8cb8c4ce148dbef144228fa9121c
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-10 14:48:46 +0200

    Imported interfaces

--- utilis.ts
+++ utilis.ts
@@ -1,3 +1,4 @@
+import { Review } from './interfaces'
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
 const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
 const userNameDisplay = document.querySelector('#user') as HTMLElement
