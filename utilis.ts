7a87c6f5eabbfaacf2879c71b5276430a2ff3f20
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-10 14:29:05 +0200

    Defined Permissions enum

--- utilis.ts
+++ utilis.ts
@@ -1,3 +1,8 @@
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
 const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
 const userNameDisplay = document.querySelector('#user') as HTMLElement
+
+enum Permissions {
+    ADMIN = 'ADMIN', 
+    READ_ONLY = 'READ_ONLY'
+}
