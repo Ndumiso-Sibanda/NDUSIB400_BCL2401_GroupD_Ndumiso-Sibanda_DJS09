dec37418fbedab4877ace3c193c48a11e369a43f
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-10 13:55:29 +0200

    Added Malia Hotel to our accommodations list!

--- index.ts
+++ index.ts
@@ -61,4 +61,15 @@ const properties : Property[] = [
         contact: [+34829374892553, 'andyluger@aol.com'],
         isAvailable: true
     },
+    {
+        image: './images/malaysian-hotel.jpeg',
+        title: 'Malia Hotel',
+        price: 35,
+        location: {
+            firstLine: 'Room 4',
+            city: 'Malia',
+            code: 45334,
+            country: 'Malaysia'
+        },
+    },
     ]
